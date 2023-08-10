ac71d8ff9e72004bccaca6ed398deae5a01b1506
itamar81 <itamarh@terasky.com>
2023-08-10 12:09:14 +0300

    add frontend

--- api-gateway/hendlers/products.go
+++ api-gateway/hendlers/products.go
@@ -1,33 +1,46 @@
-package hendlers
-
-func NewProducts(l *log.Logger) Products{
-	return &Products{l}
-}
-
-func (p *Products) ServerHTTP(rw http.ResponseWriter , r *http.Request){
-	if r.Method == http.MethodGet {
-		
-	}
-
+package handlers
 
 import (
 	"log"
 	"net/http"
-	"github.com/itamar81/api-gateway/data"
+
+	"github.com/itamar81/micro-services-arch/api-gateway/data"
 )
 
-
-type Products struct{
+// Products is a http.Handler
+type Products struct {
 	l *log.Logger
 }
-		p.getProducts(wr,r)
+
+// NewProducts creates a products handler with the given logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
+// interface
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle the request for a list of products
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
 		return
 	}
 
+	// catch all
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (p *Products) getProducts(rw http.ResponseWriter , r *http.Request){
-	p.l.Println("Handle get Products")
-	listProducts := data.gatewayProducts
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
 
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
 }
