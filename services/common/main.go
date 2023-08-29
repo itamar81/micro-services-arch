c3db5420bbc5f4f0506a2185b3ba2ef67ff28c08
itamar81 <itamarh@terasky.com>
2023-08-29 11:41:32 +0300

    common

--- services/common/main.go
+++ services/common/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "os"
+import (
+	"os"
+	"log"
+)
 
-func getEnv(key, fallback string) string {
+func GetEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
 	return fallback
+}
+const(
+	MYSQL_DRIVER_NAME = "mysql"
+)
+
+
+func FailOnError(l *log.Logger, err error, msg string) {
+	if err != nil {
+		l.Panicf("%s: %s", msg, err)
+	}
+}
+func PanicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
